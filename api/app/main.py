21205a7e69fd7824372d9d223a0c7d44cc3d36f0
Liam McKenna <liam.mckenna04@gmail.com>
2023-09-17 09:44:40 -0400

    removed misc comments

--- api/app/main.py
+++ api/app/main.py
@@ -6,5 +6,4 @@ app = FastAPI()
 @app.get("/") # all logic should be here, isolate gpu heavy functions to gpu_api
 async def root():
     return {"message": "Hello Wdorld"}
-# https://github.com/ultralytics/yolov5/discussions/5872
 
